a428685673f1953abc719b385359dc3a4a1e655e
RiiXXD <thereforeme06@gmail.com>
2024-02-14 23:43:43 +0530

    msg:update bug resolved

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -2,16 +2,15 @@ import './App.css';
 import React from 'react';
 import RecipeForm  from './Pages/Form';
 import AllRoutes from './routers/Routers';
-import { ChakraProvider } from '@chakra-ui/react'
+
 function App() {
-  return (
-    <ChakraProvider>
-    <div className="App" >
+  return (<div className="App" >
     <AllRoutes/>
     {/* <RecipeForm/> */}
     </div>
-    </ChakraProvider>
-    
+   
+   
+
   );
 }
 
