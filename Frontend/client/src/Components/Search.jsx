a36fa7a521ab8883004bfc9d92299a619edba8cd
RiiXXD <thereforeme06@gmail.com>
2024-02-26 02:40:19 +0530

    CREATING SEARCH ENDPOINT

--- Frontend/client/src/Components/Search.jsx
+++ Frontend/client/src/Components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Text,Textarea,Button,Box,Flex,InputRightElement,SearchIcon
     ,FormControl,Input,FormLabel,useToast, Checkbox, CheckboxGroup, Stack,FormHelperText, Heading, InputGroup
@@ -6,14 +6,30 @@ import {
 import { useNavigate } from 'react-router-dom';
 import { FaSearch } from "react-icons/fa";
 export default function Search() {
-  const[key,setKey]=useState('');
+  const[key,setKey]=useState();
+  const[debouncedValue,setDebouncedValue]=useState('');
   const searchKey=(e)=>{
-   setKey(e.target.value);
-   console.log(key)
+  setKey(e.target.value);
   }
+
+  useEffect(()=>{
+    const id=setTimeout(()=>{
+      setDebouncedValue(key);
+    },1000);
+    
+    return ()=>{
+      clearTimeout(id);
+    }
+ 
+  },[key]);
+  console.log(debouncedValue===""?"nothing":debouncedValue);
+  
+
+  
+
     return(
         <InputGroup w="40%">
-          <Input variant='filled' bg="white" placeholder='Search' onChange={(e)=>{searchKey(e)}} />
+          <Input variant='filled' bg="white" placeholder='Search' onChange={(e)=>{searchKey(e);}} />
           <InputRightElement pointerEvents='none'>
       <FaSearch color='gray.300' />
     </InputRightElement>
